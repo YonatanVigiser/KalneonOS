b05dcd3a9a9f2fe3d395ba7851d3f00bedc0dfe1
Yonatan Vigiser <yonatanvigiser@gmail.com>
2025-05-03 21:24:48 +0300

    Remove unnecery lines in kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,5 @@
 #include <stdint.h>
 
-__attribute__((section(".text._start"), used, naked))
 void _start(void) {
   __asm__ volatile (
     "call kernel_main\n"
@@ -9,10 +8,5 @@ void _start(void) {
 }
 
 void kernel_main(void) {
-  uint32_t c = 0;
-  uint32_t* p = (uint32_t*) 0x200000;
-  while (c <= 10) {
-    *(p) = c;
-    c++;
-  }
+
 }
