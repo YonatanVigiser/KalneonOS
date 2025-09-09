b3853d2d2310d58b0b641e9b53ec04380e773f7d
yonatan vigiser <yonatanvigiser@gmail.com>
2025-09-09 10:58:12 +0300

    Work on memmap

--- kernel/src/arch/x86/memory_map.rs
+++ kernel/src/arch/x86/memory_map.rs
@@ -37,8 +37,8 @@ impl TryFrom<u32> for MemoryType {
 
 #[derive(Debug, Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
 pub struct MemoryMapEntry {
-    pub start: u32,
-    pub end: u32,
+    pub start: usize,
+    pub end: usize,
     pub mem_type: MemoryType,
 }
 
@@ -50,13 +50,28 @@ impl MemoryMap {
     pub fn from_bios_mmap(bios_mmap: &[boot_info::MemoryMapEntry]) -> Self {
         let mut mmap = Self([MemoryMapEntry { start: 0, end: 0, mem_type: MemoryType::Reserved }; MMAP_SIZE]);
         for (count, entry) in bios_mmap.iter().enumerate() {
-            mmap.0[count].start = entry.base as u32;
-            mmap.0[count].end = entry.base as u32 + entry.length as u32;
+            mmap.0[count].start = entry.base as usize;
+            mmap.0[count].end = entry.base as usize + entry.length as usize;
             mmap.0[count].mem_type = entry.mem_type.try_into().unwrap_or(MemoryType::Reserved);
         }
         mmap.0.sort_unstable();
+        let mut holes_counter = 0;
+        for entry_index in 1..bios_mmap.len() {
+            if mmap.0[entry_index - 1].end < mmap.0[entry_index].start {
+                mmap.0[bios_mmap.len() + holes_counter] = MemoryMapEntry {
+                    start: mmap.0[entry_index - 1].end,
+                    end: mmap.0[entry_index].start,
+                    mem_type: MemoryType::Reserved,
+                };
+                holes_counter += 1;
+            }
+            if mmap.0[entry_index - 1].end > mmap.0[entry_index].start {
+                if mmap.0[entry_index - 1].mem_type.is_usable() {
+                } else {
+                }
+            }
+        }
         mmap
-
     }
 
     pub fn size() -> usize {
