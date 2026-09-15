685c5f3cdeea7d2d2e2f76f092710f7db679b934
yonatan vigiser <yonatanvigiser@gmail.com>
2026-09-15 16:16:00 +0300

    Fix SMP bug where all low mem was allocated to the heap

--- src/memory/frame_allocator.rs
+++ src/memory/frame_allocator.rs
@@ -2,14 +2,19 @@ use super::{FrameSize, MemoryMap, MemoryType};
 use crate::common::traits::Indexable;
 
 use bitvec::prelude::{BitVec, Lsb0, bitvec};
-use x86_64::structures::paging::{
+use x86_64::{PhysAddr, structures::paging::{
     FrameAllocator as Allocator, FrameDeallocator as Deallocator, PageSize, Size4KiB,
     frame::{PhysFrame, PhysFrameRange},
+}};
+
+pub const LOW_MEMORY_LIMIT: PhysFrame = unsafe {
+    PhysFrame::from_start_address_unchecked(PhysAddr::new(0x100_000))
 };
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct BitmapAllocator {
     frames_bitmap: BitVec<usize, Lsb0>, // 0 is free, 1 is allocated / reserved
+    frame_limit: PhysFrame, // Not included
     next_search_hint: usize,
 }
 
@@ -24,6 +29,7 @@ impl BitmapAllocator {
             .end
             .as_index();
         let mut frames_bitmap = bitvec![1; total_frames];
+        let frame_limit = PhysFrame::from_index(total_frames);
         for usable_region in mmap
             .entires()
             .iter()
@@ -34,6 +40,7 @@ impl BitmapAllocator {
         }
         let mut allocator = Self {
             frames_bitmap,
+            frame_limit,
             next_search_hint: 0,
         };
         unsafe {
@@ -54,6 +61,19 @@ impl BitmapAllocator {
             "mark as used was called on an out of bound frame range"
         );
         self.frames_bitmap[range.start.as_index()..range.end.as_index()].fill(true);
+    }
+
+    pub fn allocate_frame_in(&mut self, range: PhysFrameRange<FrameSize>) -> Option<PhysFrame<FrameSize>> {
+        let range = range.start.as_index()..range.end.as_index();
+        let index = range.start + self.frames_bitmap.get(range)?.first_zero()?;
+        self.frames_bitmap.set(index, true);
+        Some(PhysFrame::from_index(index))
+    }
+
+    pub fn allocate_frame_below(&mut self, limit: PhysFrame) -> Option<PhysFrame<FrameSize>> {
+        let limit = limit.min(self.frame_limit);
+        let range = PhysFrameRange { start: PhysFrame::from_index(0), end: limit };
+        self.allocate_frame_in(range)
     }
 
     pub fn free_frames_count(&self) -> usize {
@@ -77,21 +97,23 @@ unsafe impl Allocator<FrameSize> for BitmapAllocator {
             self.next_search_hint += 1;
             return Some(PhysFrame::from_index(self.next_search_hint - 1));
         }
-        let index = self.frames_bitmap.first_zero()?;
-        self.frames_bitmap.set(index, true);
-        self.next_search_hint = index + 1;
-        Some(PhysFrame::from_index(index))
+        let lower_range = PhysFrameRange { start: PhysFrame::from_index(0), end: LOW_MEMORY_LIMIT };
+        let upper_range = PhysFrameRange { start: LOW_MEMORY_LIMIT, end: self.frame_limit };
+        let frame = self.allocate_frame_in(upper_range)
+            .or_else(|| self.allocate_frame_in(lower_range))?;
+        self.next_search_hint = frame.as_index();
+        Some(frame)
     }
 }
 
 impl Deallocator<FrameSize> for BitmapAllocator {
     unsafe fn deallocate_frame(&mut self, frame: PhysFrame<Size4KiB>) {
         assert!(
-            self.frames_bitmap.get(frame.as_index()).is_none(),
+            self.frames_bitmap.get(frame.as_index()).is_some(),
             "dealloc was called on an out of bound frame!"
         );
         debug_assert!(
-            !self.frames_bitmap.get(frame.as_index()).unwrap(),
+            self.frames_bitmap.get(frame.as_index()).unwrap(),
             "dealloc was called on a frame that was free in frame allocator"
         );
         self.frames_bitmap.set(frame.as_index(), false);
